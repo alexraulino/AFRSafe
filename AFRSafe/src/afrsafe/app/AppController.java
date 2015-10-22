2697bbabb88ea8199f5269a12df43b26cdf64b76
Alex Felipe Raulino <alex_raulino@outlook.com>
2015-10-22 18:46:51 -0200

    Inicio Repositorio

---
+++ AFRSafe/src/afrsafe/app/AppController.java
@@ -0,0 +1,21 @@
+package afrsafe.app;
+
+import android.app.Application;
+
+public class AppController extends Application {
+
+	public static final String TAG = AppController.class.getSimpleName();
+
+	private static AppController mInstance;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mInstance = this;
+	}
+
+	public static synchronized AppController getInstance() {
+		return mInstance;
+	}
+
+}
